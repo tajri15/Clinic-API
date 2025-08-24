2b43b77e4eed004aeadfbc76d9825eb5971a7658
Tajri Ramal Muzi <ramalmuzitajri@gmail.com>
2025-08-24 17:41:52 +0700

    Initial commit: Generate Spring Boot project structure

---
+++ src/main/java/com/portofolio/clinicapi/ClinicapiApplication.java
@@ -0,0 +1,13 @@
+package com.portofolio.clinicapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicapiApplication.class, args);
+	}
+
+}
